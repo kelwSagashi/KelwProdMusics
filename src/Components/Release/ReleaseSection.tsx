a2016ec79332485c62c440fb34b9e3c441d2a16b
kelw.prod@gmail.com <kelw.prod@gmail.com>
2024-07-23 17:39:36 -0300

    Add ReleaseSection component

---
+++ src/Components/Release/ReleaseSection.tsx
@@ -0,0 +1,30 @@
+import { NavBarLink, NavBarUl } from "../NavBar/NavBarLink"
+
+function ReleaseSection() {
+  return (
+    <div className="h-auto flex flex-col items-center mt-6 md:mt-20">
+        <div className="p-4 w-full flex justify-between items-center">
+            <div>
+                <h1 className="text-2xl sm:text-4xl md:text4xl text-center tracking-wide">
+                    Lançamentos
+                </h1>
+            </div>
+            <div className="h-full">
+                <button className="text-center">
+                    <NavBarUl>
+                        <NavBarLink className='px-3 py-2 rounded-md 
+                            hover:transition-all hover:duration-200
+                            hover:bg-gradient-to-r hover:from-yellow-500 
+                            hover:to-fuchsia-700' 
+                        href='/'>
+                            Mais Lançamentos
+                        </NavBarLink>
+                    </NavBarUl>
+                </button>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default ReleaseSection
