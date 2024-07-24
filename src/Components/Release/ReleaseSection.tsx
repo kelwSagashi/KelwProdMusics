f7e30436cf4cf047d4ccd6b2a5ac8c8504a7d6bc
kelw.prod@gmail.com <kelw.prod@gmail.com>
2024-07-23 22:22:37 -0300

    add map function to list the Music Cards

--- src/Components/Release/ReleaseSection.tsx
+++ src/Components/Release/ReleaseSection.tsx
@@ -1,30 +1,43 @@
+import CardPlayer from "../CardPlayer/CardPlayer"
+import { CardsPropsOfMUsics } from "../CardPlayer/types"
 import { NavBarLink, NavBarUl } from "../NavBar/NavBarLink"
 
 function ReleaseSection() {
-  return (
-    <div className="h-auto flex flex-col items-center mt-6 md:mt-20">
-        <div className="p-4 w-full flex justify-between items-center">
-            <div>
-                <h1 className="text-2xl sm:text-4xl md:text4xl text-center tracking-wide">
-                    Lançamentos
-                </h1>
-            </div>
-            <div className="h-full">
-                <button className="text-center">
-                    <NavBarUl>
-                        <NavBarLink className='px-3 py-2 rounded-md 
+    return (
+        <>
+            <div className="h-auto flex flex-col items-center mt-6 md:mt-20">
+                <div className="p-4 w-full flex justify-between items-center">
+                    <div>
+                        <h1 className="text-2xl sm:text-4xl md:text4xl text-center tracking-wide">
+                            Lançamentos
+                        </h1>
+                    </div>
+                    <div className="h-full">
+                        <button className="text-center">
+                            <NavBarUl>
+                                <NavBarLink className='px-3 py-2 rounded-md 
                             hover:transition-all hover:duration-200
                             hover:bg-gradient-to-r hover:from-yellow-500 
-                            hover:to-fuchsia-700' 
-                        href='/'>
-                            Mais Lançamentos
-                        </NavBarLink>
-                    </NavBarUl>
-                </button>
+                            hover:to-fuchsia-700'
+                                    href='/'>
+                                    Mais Lançamentos
+                                </NavBarLink>
+                            </NavBarUl>
+                        </button>
+                    </div>
+                </div>
             </div>
-        </div>
-    </div>
-  )
+
+            <div className="flex flex-wrap gap-4 p-4">
+                {CardsPropsOfMUsics.map((value, i) => (
+                    <CardPlayer key={i} 
+                    author={value.author} 
+                    name={value.name} 
+                    img={value.img} />
+                ))}
+            </div>
+        </>
+    )
 }
 
 export default ReleaseSection
