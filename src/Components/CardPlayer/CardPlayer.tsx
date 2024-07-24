786a2477f1ebfbf1ffa50b51c11d40dc3aa1e2a9
kelw.prod@gmail.com <kelw.prod@gmail.com>
2024-07-24 01:38:23 -0300

    Changing CardPlayer props

--- src/Components/CardPlayer/CardPlayer.tsx
+++ src/Components/CardPlayer/CardPlayer.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
-import { CardMusicPlayerT } from './types'
+import { PlayerProps } from '../Player/types'
 
-const CardPlayer = ({ img, name, author }: CardMusicPlayerT) => {
+const CardPlayer: React.FC<PlayerProps> = ({ title, artist, thumbnail }) => {
     return (
         <div className='w-[200px] h-[250px] flex flex-col cursor-pointer group'>
-            <img src={img} className='rounded-t-md group-hover:scale-110
+            <img src={thumbnail} className='rounded-t-md group-hover:scale-110
             transform transition-transform duration-300 ease-in-out
-            group-hover:drop-shadow-xl'></img>
-            <span className='font-semibold mt-4'>{name}</span>
-            <span className='font-thin text-neutral-300'>{author}</span>
+            group-hover:drop-shadow-xl' />
+            <span className='font-semibold mt-4'>{title}</span>
+            <span className='font-thin text-neutral-300'>{artist}</span>
         </div>
     )
 }
