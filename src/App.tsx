2f2956bb696aee0d04075cc37360391bf1e11f40
kelw.prod@gmail.com <kelw.prod@gmail.com>
2024-07-24 01:37:12 -0300

    removing Routes from main and adding Home page Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,33 @@
+import { Outlet, Route, Routes } from 'react-router-dom'
 import './App.css'
+import CoverSection from './Components/Covers/CoverSection'
 import HeroSection from './Components/Hero/HeroSection'
 import NavBar from './Components/NavBar/NavBar'
 import ReleaseSection from './Components/Release/ReleaseSection'
+import Musics from './Pages/Musics'
+import Music from './Pages/Music'
 
 function App() {
   return (
     <>
-      <NavBar/>
-      <HeroSection/>
-      <ReleaseSection/>
+      <NavBar />
+      <Outlet/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/Musicas' element={<Musics/>}/>
+        <Route path='/Music/:id' element={<Music/>}/>
+      </Routes>
     </>
   )
 }
 
+function Home() {
+  return (
+    <>
+      <HeroSection />
+      <ReleaseSection />
+      <CoverSection />
+    </>
+  )
+}
 export default App
